28553e27c6c8870d36af79a7cb164d18166a6395
Alice Bilha <aliceilhaa@gmail.com>
2022-08-20 11:15:11 -0300

    first program

---
+++ firstprogram.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+
+{
+
+int age{}; //variable type: int
+
+    std::cout<< "How old are you?" <<std::endl;
+    std::cin>> age;
+    std::cout<<"wow, we are the same age! I'm also " << age << " years old! How about that!" <<std::endl;
+    
+}
