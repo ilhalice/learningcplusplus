51b080aa6e403b95736c2e153fcd839f73dcca80
Alice Bilha <aliceilhaa@gmail.com>
2022-08-21 10:48:48 -0300

    second program

---
+++ secondtry.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number{};
+    int number2{8};
+
+    cout<< "Peter: -What's your favorite number?"<<endl;
+    cin>> number;
+
+    cout<< "John: -What about yours?" <<endl;
+    
+    cout<< "Thank's for asking! My favorite number is " << number2 << endl;
+}
